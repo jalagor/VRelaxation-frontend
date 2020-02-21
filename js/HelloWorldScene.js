a06391189fcdaf040a231f823dc60b67bf289a7a
jalagor <johnrossrogla@gmail.com>
2020-02-21 16:14:05 -0700

    edited state

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -18,7 +18,7 @@ export default class HelloWorldScene extends Component {
   constructor() {
     super();
     this.state = {
-      greeting: "sup bitch",
+      greeting: "sup",
     
     } 
     this._onHovering = this._onHovering.bind(this);
@@ -30,9 +30,7 @@ export default class HelloWorldScene extends Component {
   render() {
     return (
       <ViroScene>
-        {/* <Viro360Image source={require('./res/guadalupe_360.jpg')} /> */}
         <ViroSkyBox color='rgb(0, 0, 0)'/>
-        {/* <ViroText text="Hello World!" width={1} height={2} position={[2, 0, -2]} rotation={[0,-55,0]} style={styles.helloWorldTextStyle} /> */}
         <ViroText text={this.state.greeting} width={1} height={2} position={[0, 0, -3]} rotation={[0,0,0]} style={styles.helloWorldTextStyle} />
         <ViroButton height={1} width={1} roatation={[0,50,0]} position={[-2, 0, -2]} onClick={this._onHovering}source={require('./res/icon_right_w.png')}/>
         <ViroVideo loop={true} source={require('./res/video.mp4')} scale={[3, 3, 0]} position={[-3, 0, -3]} rotation={[0,50,0]}/>
